c086f9be8f0c2407c819af8ec0393755c9d9be6d
liangwei0101 <liangwei0101@users.noreply.github.com>
2018-11-28 19:47:41 +0800

    增加代码

---
+++ eureka-client/src/main/java/com/liangwei/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,14 @@
+package com.liangwei.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+}
